001837ffbea37ecf409cefe9995687cb6787deaa
Flipapa <roamer2010x@gmail.com>
2021-07-09 16:02:14 +0800

    refactor: modify all function to fit mongoose

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
-const restaurantList = require('./restaurant.json')
+const Restaurant = require('./models/restaurant')
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/restaurant_list', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -20,8 +20,10 @@ db.once('open', () => {
 
 // 列出不重複分類
 const categories = new Set()
-restaurantList.results.forEach(restaurant =>
+Restaurant.find().lean().then((restaurants) => {
+  restaurants.forEach(restaurant =>
   categories.add(restaurant.category))
+})
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -29,27 +31,39 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index', { restaurants: restaurantList.results, category: categories })
+  Restaurant.find()
+    .lean()
+    .then(restaurants => res.render('index', { restaurants, category: categories }))
+    .catch(error => console.error(error))
 })
 
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword.trim()
-  const restaurants = restaurantList.results.filter((restaurant) => {
-    return restaurant.name.toLocaleLowerCase().includes(keyword.toLocaleLowerCase()) || restaurant.category.toLocaleLowerCase().includes(keyword.toLocaleLowerCase())
-  })
-  const recRestaurants = restaurantList.results.filter((restaurant) => {
-    return restaurant.rating > 4.5
-  })
-  if (restaurants.length === 0) {
-    res.render('find_no_result',{ keyword: keyword, category:categories, recommends:recRestaurants })
-  } else {
-    res.render('index', { restaurants: restaurants, keyword: keyword, category: categories })
-  }
+
+  Restaurant.find()
+    .lean()
+    .then((restaurants) => {
+      restaurantSearch = restaurants.filter((restaurant) => {
+        return restaurant.name.toLocaleLowerCase().includes(keyword.toLocaleLowerCase()) || restaurant.category.toLocaleLowerCase().includes(keyword.toLocaleLowerCase())
+      })
+      recRestaurants = restaurants.filter((restaurant) => {
+        return restaurant.rating > 4.5
+      })
+      if (restaurantSearch.length === 0) {
+        res.render('find_no_result', { keyword, category: categories, recommends: recRestaurants })
+      } else {
+        res.render('index', { restaurants: restaurantSearch, keyword, category: categories })
+      }
+    })
+    .catch(error => console.log(error))
 })
 
 app.get('/restaurants/:restaurant_id', (req, res) => {
-  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
-  res.render('show', { restaurant: restaurant })
+  const id = req.params.restaurant_id
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurant) => res.render('show', { restaurant }))
+    .catch(error => console.log(error))
 })
 
 app.listen(port, () => {
