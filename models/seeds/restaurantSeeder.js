04fcbbc29903941c0717dbf00aae52279759b6d2
Flipapa <roamer2010x@gmail.com>
2021-07-18 02:45:51 +0800

    feat: add auto exit after npm run seed

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,22 +1,13 @@
 const Restaurant = require('../restaurant')
 const rawData = require('./restaurant.json')
 const restaurantList = rawData.results
-require('../../config/mongoose')
+const db = require('../../config/mongoose')
 
 db.once('open', () => {
   console.log('processing seeder!')
-  for (const restaurant of restaurantList) {
-    Restaurant.create({
-      name: restaurant.name,
-      name_en: restaurant.name_en,
-      category: restaurant.category,
-      image: restaurant.image,
-      location: restaurant.location,
-      phone: restaurant.phone,
-      google_map: restaurant.google_map,
-      rating: restaurant.rating,
-      description: restaurant.description
+  Restaurant.create(restaurantList)
+    .then(() => {
+      db.close()
+      console.log('mongodb seeder processing done!')
     })
-  }
-  console.log('mongodb seeder processing done!')
 })
